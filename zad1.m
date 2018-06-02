3203f52b303da77c0b935a3419d6e4ef6d880b58
PabloOb <pawel.obrepalski@gmail.com>
2018-06-02 15:42:48 +0200

    fixd zad1

--- zad1.m
+++ zad1.m
@@ -1,42 +1,42 @@
-clear all
+clear 
 clc
 format long
 
-syms x y
-r1(x,y)=x.^2+y.^2+2.*x-324;
-r2(x,y)=2.*x.*y+9.*y-43;
+syms x1 x2
+r1(x1, x2) = x1^2 + 8 * x2^2 + 2 * x1 - 655;
+r2(x1, x2) = 16 * x1 * x2 + 6 * x2 - 1456;
 
-eps=0.1;
-x0=[17.3;0.8];
+eps = 0.1;
+x0 = [11.3; 7.8];
 
-a=x0(1);
-b=x0(2);
-i=0;
+a = x0(1);
+b = x0(2);
+i = 0;
 
-f(x,y)=.5.*(r1.*r1+r2.*r2);
-g(x,y)=gradient(f);
-h(x,y)=hessian(f);
+f(x1, x2) = .5 * (r1 .* r1 + r2 .* r2);
+g(x1, x2) = gradient(f);
+h(x1, x2) = hessian(f);
 
-g1=g(a,b);
-norm=sqrt(g1(1).^2+g1(2).^2);
-norm=double(norm);
+g1 = g(a, b);
+norm = sqrt(g1(1)^2 + g1(2)^2);
+norm = double(norm);
 
 disp(['x' num2str(i) ' = [' num2str(a) ',' num2str(b) ']; ||grad f(x' num2str(i) ')|| =' num2str(norm) ])
 
-xp=x0;
+xp = x0;
 
 while eps < norm
-      dp = - inv(h(a,b))*g(a,b);
+      dp = - inv(h(a, b)) * g(a, b);
       xn = xp + dp;
-      xn=double(xn);
+      xn = double(xn);
      
-      a=xn(1);
-      b=xn(2);
-      g1=g(a,b);
-      norm=sqrt(g1(1).^2+g1(2).^2);
-      norm=double(norm);
-      i=i+1;
+      a = xn(1);
+      b = xn(2);
+      g1 = g(a, b);
+      norm = sqrt(g1(1)^2 + g1(2)^2);
+      norm = double(norm);
+      i = i+1;
       
       disp(['x' num2str(i) ' = [' num2str(a) ',' num2str(b) ']; ||grad f(x' num2str(i) ')|| =' num2str(norm) ])
-      xp=xn;
+      xp = xn;
 end
