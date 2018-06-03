00c9091abf0f19ead9ec6e4e6504b97efffb4f30
PabloOb <pawel.obrepalski@gmail.com>
2018-06-03 15:48:50 +0200

    changed data in 2 and 4

--- zad2.m
+++ zad2.m
@@ -1,63 +1,64 @@
-clear all
+clear
 clc
 format long
 
-syms x y z alfa
-f(x,y,z)=28*x.^2-2.*x.*y+y.^2-z-y.*z+z.^2;
-eps=0.01;
-x0=[1;0;0];
+syms x1 x2 x3 alfa
+f(x1, x2, x3) = 19 * x1^2 - 2 * x1 * x2 + x2^2 - x3 - x2 * x3 + 8 * x3^2;
 
-a=x0(1);
-b=x0(2);
-c=x0(3);
+eps = 0.01;
+x0 = [1; 0; 0];
 
-g(x,y,z)=gradient(f);
+a = x0(1);
+b = x0(2);
+c = x0(3);
+
+g(x1, x2, x3) = gradient(f);
 B0 = eye(3);
-i=0;
+i = 0;
 
-g1=g(a,b,c);
-norm=sqrt(g1(1).^2+g1(2).^2 +g1(3).^2);
-norm=double(norm);
+g1 = g(a, b, c);
+norm = sqrt(g1(1)^2 + g1(2)^2 + g1(3)^2);
+norm = double(norm);
 
 disp(['x' num2str(i) ' = [' num2str(a, '%4.3f') ',' num2str(b, '%4.3f') ',' num2str(c, '%4.3f') ']; ||grad f(x' num2str(i) ')|| =' num2str(norm, '%4.6f') ])
 
-xp=x0;
-B=B0;
+xp = x0;
+B = B0;
 
 
 while eps < norm
 
-      dp = - B*g(a,b,c);
-      xn = xp + alfa.*dp;
+      dp = - B * g(a,b,c);
+      xn = xp + alfa * dp;
       
       %szukanie optymalnego kroku
-      f_krok=f(xn(1),xn(2),xn(3));
-      eqn = gradient(f_krok)==0;
+      f_krok = f(xn(1), xn(2), xn(3));
+      eqn = gradient(f_krok) == 0;
       alfa_star = solve(eqn, alfa);
       
       %nowe przyblizenie
-      xn = xp + alfa_star.*dp;
-      xn=double(xn);
+      xn = xp + alfa_star * dp;
+      xn = double(xn);
      
-      a=xn(1);
-      b=xn(2);
-      c=xn(3);
+      a = xn(1);
+      b = xn(2);
+      c = xn(3);
       
-      g0=g1;
-      g1=g(a,b,c);
-      q=g1-g0;
-      norm=sqrt(g1(1).^2+g1(2).^2+g1(3).^2);
-      norm=double(norm);
-      i=i+1;
+      g0 = g1;
+      g1 = g(a, b, c);
+      q = g1 - g0;
+      norm = sqrt(g1(1)^2 + g1(2)^2 + g1(3)^2);
+      norm = double(norm);
+      i = i + 1;
       
       disp(['x' num2str(i) ' = [' num2str(a, '%4.3f') ',' num2str(b, '%4.3f') ',' num2str(c, '%4.3f') ']; ||grad f(x' num2str(i) ')|| =' num2str(norm, '%4.6f') ])
       
-      D=dp'*q;
-      A=dp*dp'./D;
-      C=q'*B*q./D;
-      E=(B*q*dp'+ dp*q'*B)/D;
+      D = dp' * q;
+      A = dp * dp' / D;
+      C = q' * B * q / D;
+      E = (B* q * dp' + dp *q' * B) / D;
       
-      dB = A*(1+C) - E;
-      B=B+dB;
-      xp=xn;
+      dB = A*(1 + C) - E;
+      B = B + dB;
+      xp = xn;
 end
--- zad4.m
+++ zad4.m
@@ -1,57 +1,57 @@
-clear all
+clear
 clc
 format long
 
-g_acc=9.81;
-m1=1;
-m2=1;
-l=1;
-F=0;
+g_acc = 9.81;
+m1 = 6;
+m2 = 8;
+l = 1;
+F = 140;
 
-syms x y 
-f(x,y)=-g_acc*l*(m1*cos(x)+2*m2*cos(x)+m2*cos(y)) - 2*F*l*(sin(x)+sin(y));
+syms x1 x2 
+f(x1, x2) = -g_acc * l * (m1 * cos(x1) + 2 * m2 * cos(x1) + m2 * cos(x2)) - 2 * F * l * (sin(x1) + sin(x2));
 
-eps=10e-8;
-x0=[pi/6;pi/3];
+eps = 10e-8;
+x0 = [pi/6; pi/3];
 
-a=x0(1);
-b=x0(2);
-i=0;
+a = x0(1);
+b = x0(2);
+i = 0;
 
-g(x,y)=gradient(f);
-h(x,y)=hessian(f);
+g(x1, x2) = gradient(f);
+h(x1, x2) = hessian(f);
 
-g1=g(a,b);
-norm=sqrt(g1(1).^2+g1(2).^2);
-norm=double(norm);
+g1 = g(a,b);
+norm = sqrt(g1(1)^2 + g1(2)^2);
+norm = double(norm);
 
 disp(['x' num2str(i) ' = [' num2str(a, '%4.3f') ',' num2str(b, '%4.3f') ']; ||grad f(x' num2str(i) ')|| =' num2str(norm, '%4.3f') ])
 
-xp=x0;
+xp = x0;
 
 while eps < norm
-      dp = - inv(h(a,b))*g(a,b);
+      dp = -inv(h(a, b))*g(a, b);
       xn = xp + dp;
-      xn=double(xn);
+      xn = double(xn);
      
-      a=xn(1);
-      b=xn(2);
-      g1=g(a,b);
-      norm=sqrt(g1(1).^2+g1(2).^2);
-      norm=double(norm);
-      i=i+1;
+      a = xn(1);
+      b = xn(2);
+      g1 = g(a,b);
+      norm = sqrt(g1(1)^2 + g1(2)^2);
+      norm = double(norm);
+      i = i + 1;
       
       disp(['x' num2str(i) ' = [' num2str(a, '%4.3f') ',' num2str(b, '%4.3f') ']; ||grad f(x' num2str(i) ')|| =' num2str(norm, '%4.3f') ])
-      xp=xn;
+      xp = xn;
 end
 
-x1=l*sin(xp(1));
-y1=-l*cos(xp(1));
-phi1=270+xp(1)*180/pi;
+x1 = l * sin(xp(1));
+y1 = -l * cos(xp(1));
+phi1 = 270 + xp(1) * 180 / pi;
 
-x2=2*l*sin(xp(1))+l*sin(xp(2));
-y2=-2*l*cos(xp(1))-l*cos(xp(2));
-phi2=270+xp(2)*180/pi;
+x2 = 2 * l * sin(xp(1)) + l * sin(xp(2));
+y2 = -2 * l * cos(xp(1)) - l * cos(xp(2));
+phi2 = 270 + xp(2) * 180 / pi;
 
- disp(['q1 = [x1, y1, phi1] = [' num2str(x1, '%4.3f') ', ' num2str(y1, '%4.3f') ', '  num2str(phi1, '%4.3f') ']' ])
- disp(['q2 = [x2, y2, phi2] = [' num2str(x2, '%4.3f') ', ' num2str(y2, '%4.3f') ', '  num2str(phi2, '%4.3f') ']' ])
+disp(['q1 = [x1, y1, phi1] = [' num2str(x1, '%4.3f') ', ' num2str(y1, '%4.3f') ', '  num2str(phi1, '%4.3f') ']' ])
+disp(['q2 = [x2, y2, phi2] = [' num2str(x2, '%4.3f') ', ' num2str(y2, '%4.3f') ', '  num2str(phi2, '%4.3f') ']' ])
